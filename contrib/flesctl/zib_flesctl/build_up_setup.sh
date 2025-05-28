baa5c6298e46dbd36b9a4467edd5ee6d4d55f3c6
Jannik Graf von Schlieffen <von.schlieffen@zib.de>
2025-05-28 18:19:45 +0200

    updating config example

--- contrib/flesctl/zib_flesctl/build_up_setup.sh
+++ contrib/flesctl/zib_flesctl/build_up_setup.sh
@@ -1,16 +1,35 @@
+function create_venv() {
+    python3 -m venv flesctrl_venv
 
-python3 -m venv flesctrl_venv
+    source flesctrl_venv/bin/activate
 
-source flesctrl_venv/bin/activate
+    pip install docopt
 
-pip install docopt
+    pip install plotext
 
-pip install plotext
+    pip install matplotlib
 
-pip install matplotlib
+    pip install seaborn
 
-pip install seaborn
+    pip install deepdiff
 
-pip install deepdiff
+    deactivate
+}
 
-deactivate
+function create_folders() {
+
+    mkdir logs
+
+    mkdir logs/general
+    
+    mkdir logs/collectl
+
+    mkdir logs/collectl/entry_nodes
+
+    mkdir logs/collectl/build_nodes
+
+    mkdir logs/collectl/tsclient
+}
+
+create_venv
+create_folders
