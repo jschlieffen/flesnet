783cef83cf7d9ad6b2a3016846e3224a5614b2ea
Jannik Graf von Schlieffen <von.schlieffen@zib.de>
2026-08-13 20:10:07 +0200

    tmp

--- contrib/flesctl/zib_flesctl/central_manager/ZIB_Timeslice_forwarding.py
+++ contrib/flesctl/zib_flesctl/central_manager/ZIB_Timeslice_forwarding.py
@@ -46,7 +46,7 @@ class Timeslice_forwarding_ZIB:
         commands = {}
         if self.Par_.use_collectl:
             commands['1'], commands['2'] = self.define_collectl_commands(collectl_logfile)
-        cm_command = f"{self.Par_.path}./timeslice_forwarder -l 1  -c {self.central_manager_ips}:{self.Par_.port} > {logfile}"
+        cm_command = f"{self.Par_.path}./timeslice_forwarder -l 1  -c {self.central_manager_ips}:{self.Par_.port} > {logfile} 2>&1 &"
         if self.Par_.use_grafana:
             cm_command += f" -m influx2:{self.Par_.influx_node_ip}:timeslice_forwarder_state:{self.Par_.influx_token}"
         commands['3'] = cm_command
@@ -74,7 +74,7 @@ class Timeslice_forwarding_ZIB:
             f"-A {node_ip}:{self.Par_.port} "
             f"-N {idx} "
             f"-i {shm_str} "
-            f"> {logfile}"
+            f"> {logfile} 2>&1 &"
         )
         if self.Par_.use_grafana:
             tsclient_command +=  f" -m influx2:{self.Par_.influx_node_ip}:tsclient_status:{self.Par_.influx_token}"
@@ -108,7 +108,7 @@ class Timeslice_forwarding_ZIB:
             f"-A {node_ip}:{self.Par_.port} "
             f"-N {idx} "
             f"-o {shm_str}?n={self.Par_.num_components}\\&descsize={self.Par_.desc_size}\\&datasize={self.Par_.data_size} "
-            f"> {logfile}"
+            f"> {logfile} 2>&1 &"
         )
         if self.Par_.use_grafana:
             tsclient_command +=  f" -m influx2:{self.Par_.influx_node_ip}:tsclient_status:{self.Par_.influx_token}"
